6e2e48f8fd5082789c189be820a44654b41bbc17
Sumeshyadav10 <ysumesh084@gmail.com>
2025-05-30 11:29:13 +0530

    change in deployment

--- E-commerce/frontend/src/api/axiosInstance.js
+++ E-commerce/frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api', // your backend API base
+  baseURL: 'https://e-mandi-ow40.onrender.com/api', // your backend API base
   withCredentials: true, // if using cookies/sessions
 });
 
--- E-commerce/htm-4/src/api/axiosInstance.js
+++ E-commerce/htm-4/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // <-- Change this to your backend URL
+  baseURL: "https://e-mandi-ow40.onrender.com/api", // <-- Change this to your backend URL
   withCredentials: true, // Important if you use cookies/session auth
 });
 
