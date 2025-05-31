2eb1533832628b033ecc1a8022c64861debb6b01
Sumeshyadav10 <ysumesh084@gmail.com>
2025-05-31 16:42:12 +0530

    render

--- E-commerce/htm-4/src/api/axiosInstance.js
+++ E-commerce/htm-4/src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://e-mandi-ow40.onrender.com/api", // <-- Change this to your backend URL
+  baseURL: "https://e-commerce-1-fkpy.onrender.com/api", // <-- Change this to your backend URL
   withCredentials: true, // Important if you use cookies/session auth
 });
 
